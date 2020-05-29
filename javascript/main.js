4fe08f7ff244c7a1ac756ae3ee714e75d2503284
Allan Mariano <genivalallan@hotmail.com.br>
2020-05-29 14:10:55 -0300

    Define basic data to set up the game

---
+++ javascript/main.js
@@ -0,0 +1,61 @@
+const canvas = document.getElementById('game-screen');
+const levelSelector = document.getElementById('level-selector');
+const ballsCounter = document.getElementById('balls-counter');
+const settingsDialog = document.getElementById('settings');
+const startBtn = document.getElementById('btn-start');
+
+ballsCounter.min = 5;
+ballsCounter.max = 100;
+ballsCounter.value = 10;
+
+let gameLevel = 'easy';
+let game = new Game(canvas);
+
+window.onkeydown = function keyboardHandler(event) {
+    switch (event.code) {
+        case 'Escape':  // Stop/Quit
+        case 'KeyQ':
+            game.stop();
+            settingsDialog.style.display = 'block';
+            break;
+        case 'KeyP':    // Pause
+            game.pause();
+            break;
+        case 'Enter':   // Start
+        case 'Space':
+            if(game.gameStatus === GameStatus.STOP) {
+                game.reset(levelSelector.value, parseInt(ballsCounter.value));
+                game.start();
+            } else if(game.gameStatus === GameStatus.PAUSE) {
+                game.start();
+            }
+            settingsDialog.style.display = 'none';
+            break;
+        case 'KeyR':    // Restart
+            game.stop();
+            game.reset(levelSelector.value, parseInt(ballsCounter.value));
+            settingsDialog.style.display = 'none';
+            game.start();
+            break;
+        case 'ArrowUp':     // Moviment
+        case 'ArrowDown':
+        case 'ArrowLeft':
+        case 'ArrowRight':
+        case 'KeyW':
+        case 'KeyS':
+        case 'KeyA':
+        case 'KeyD':
+            if(game.gameStatus === GameStatus.PLAY) {
+                game.player.move(event.code, game.screenSize);
+            }
+            break;
+        default:
+            break;
+    }
+};
+
+startBtn.onclick = function () {
+    game.reset(levelSelector.value, parseInt(ballsCounter.value));
+    settingsDialog.style.display = 'none';
+    game.start();
+};
