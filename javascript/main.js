a50504e43e00c8df38b99853ae35189de3a24605
Allan Mariano <genivalallan@hotmail.com.br>
2020-05-29 23:00:59 -0300

    Implement game score GUI in the header section
    - Defines reference to GUI balls count, elapsed time and level title elements
    - Implement update function to print the game score on the screen
    - Implement levelSelector onclick event handler to update the level text

--- javascript/main.js
+++ javascript/main.js
@@ -4,13 +4,25 @@ const ballsCounter = document.getElementById('balls-counter');
 const settingsDialog = document.getElementById('settings');
 const startBtn = document.getElementById('btn-start');
 const settingsCloseBtn = document.getElementById('settings-close-btn');
+const ballsScore = document.getElementById('balls');
+const timeScore = document.getElementById('time');
+const levelTitle = document.getElementById('level-title');
 
 ballsCounter.min = 5;
 ballsCounter.max = 100;
 ballsCounter.value = 10;
 
 let gameLevel = 'easy';
-let game = new Game(canvas);
+let game = new Game(canvas, update);
+
+function update(ballsCount, time) {
+    ballsScore.textContent = ballsCount.toString();
+    timeScore.textContent = time + ' s';
+
+    if (ballsCount === 0) {
+        settingsDialog.style.display = 'block';
+    }
+}
 
 window.onkeydown = function keyboardHandler(event) {
     switch (event.code) {
@@ -26,6 +38,7 @@ window.onkeydown = function keyboardHandler(event) {
         case 'Space':
             if(game.gameStatus === GameStatus.STOP) {
                 game.reset(levelSelector.value, parseInt(ballsCounter.value));
+                update(ballsCounter.value, 0);
                 game.start();
             } else if(game.gameStatus === GameStatus.PAUSE) {
                 game.start();
@@ -36,6 +49,7 @@ window.onkeydown = function keyboardHandler(event) {
             game.stop();
             game.reset(levelSelector.value, parseInt(ballsCounter.value));
             settingsDialog.style.display = 'none';
+            update(ballsCounter.value, 0);
             game.start();
             break;
         case 'ArrowUp':     // Moviment
@@ -58,9 +72,28 @@ window.onkeydown = function keyboardHandler(event) {
 startBtn.onclick = function () {
     game.reset(levelSelector.value, parseInt(ballsCounter.value));
     settingsDialog.style.display = 'none';
+    update(ballsCounter.value, 0);
     game.start();
 };
 
 settingsCloseBtn.onclick = function () {
     settingsDialog.style.display = 'none';
+};
+
+levelSelector.onchange = function () {
+    levelTitle.textContent = levelSelector.value;
+
+    if (levelSelector.value === 'easy') {
+        levelTitle.style.color = 'cornflowerblue';
+        levelTitle.style.textShadow = '0 0 5px blue';
+    } else if (levelSelector.value === 'normal') {
+        levelTitle.style.color = 'gold';
+        levelTitle.style.textShadow = '0 0 5px yellow';
+    } else if (levelSelector.value === 'hard'){
+        levelTitle.style.color = 'darkred';
+        levelTitle.style.textShadow = '0 0 5px red';
+    } else {
+        levelTitle.style.color = 'white';
+        levelTitle.style.textShadow = '0 0 5px white';
+    }
 };
